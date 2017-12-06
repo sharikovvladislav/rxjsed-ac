ba6343edac25c44d8dcbd6ea7379168654c37b98
Vladislav.Sharikov <vladislav.sharikov@billing.ru>
2017-12-06 09:14:34 +0300

    formatting and remove extra stuff

--- common.js
+++ common.js
@@ -5,76 +5,75 @@ const countriesNames = allCountries.map(country => country.name);
 let counter = 0;
 
 const request = (query = '') => {
-	const defer = promiseDefer();
+  const defer = promiseDefer();
 
-	const timer = setTimeout(() => {
-		const result =
-		  query.length > 0 ? countriesNames.filter(filter(query)) : [];
-		defer.resolve(result);
-	// }, 5000);
-	}, counter === 0 ? 5000 : 100);
+  const timer = setTimeout(() => {
+    const result =
+        query.length > 0 ? countriesNames.filter(filter(query)) : [];
+    defer.resolve(result);
+    // }, 5000);
+  }, counter === 0 ? 5000 : 100);
 
-	counter++;
+  counter++;
 
-	return {
-		promise: defer.promise,
-		cancel: function () {
-			clearTimeout(timer);
-			defer.reject('CANCELED');
-		}
-	};
+  return {
+    promise: defer.promise,
+    cancel: function () {
+      clearTimeout(timer);
+      defer.reject('CANCELED');
+    }
+  };
 
-	function filter(query) {
-		return function (countryName) {
-			return countryName.toLowerCase().startsWith(query.toLowerCase())
-		};
-	}
+  function filter(query) {
+    return function (countryName) {
+      return countryName.toLowerCase().startsWith(query.toLowerCase())
+    };
+  }
 };
 const clearResult = element => element.innerHTML = '';
 
 const showResult = (items, element) => {
-	if (items.length === 0) {
-		element.innerHTML = 'empty';
-		return;
-	}
+  if (items.length === 0) {
+    element.innerHTML = 'empty';
+    return;
+  }
 
-	const ul = document.createElement('ul');
+  const ul = document.createElement('ul');
 
-	for (let item of items) {
-		const li = document.createElement('li');
-		li.textContent = item;
-		ul.appendChild(li);
-	}
+  for (let item of items) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    ul.appendChild(li);
+  }
 
-	element.appendChild(ul);
+  element.appendChild(ul);
 };
-
 
 
 const debounce = (fn, timeout = 300) => {
   let timer = null;
 
   return function (...args) {
-	  if (timer) {
-		  clearTimeout(timer);
-	  }
+    if (timer) {
+      clearTimeout(timer);
+    }
 
-	  const ctx = this;
-	  timer = setTimeout(function () {
-		fn.apply(this, args);
-	  }, timeout);
+    const ctx = this;
+    timer = setTimeout(function () {
+      fn.apply(this, args);
+    }, timeout);
   }
 }
 
 function promiseDefer() {
-	var reject = void 0;
-	var resolve = void 0;
-	var promise = new Promise(function (givenResolve, givenReject) {
-	  resolve = givenResolve;
-	  reject = givenReject;
-	});
-	// $FlowIgnore: We have set the values in promise constructor, but flow doesn't know
-	return { promise: promise, resolve: resolve, reject: reject };
+  var reject = void 0;
+  var resolve = void 0;
+  var promise = new Promise(function (givenResolve, givenReject) {
+    resolve = givenResolve;
+    reject = givenReject;
+  });
+  // $FlowIgnore: We have set the values in promise constructor, but flow doesn't know
+  return {promise: promise, resolve: resolve, reject: reject};
 }
 
-export { request, clearResult, showResult, debounce };
+export {request, clearResult, showResult, debounce};
--- rxjs-ed.js
+++ rxjs-ed.js
@@ -1,57 +1,57 @@
-import { request, clearResult, showResult } from './common.js';
+import {request, clearResult, showResult} from './common.js';
 
 const className = 'rxjsed-ac';
 const input = document.querySelector(`div.${className} > input`);
 const results = document.querySelector(`div.${className} > .results`);
 
-// const input$ = Rx.Observable.fromEvent(input, 'keyup');
+const input$ = Rx.Observable.fromEvent(input, 'keyup');
 
-// // input$.subscribe(data => console.log(data));
+// input$.subscribe(data => console.log(data));
 
-// const values$ = input$.map(event => event.target.value);
+const values$ = input$.map(event => event.target.value);
 
-// // values$.subscribe(data => console.log(data));
+// values$.subscribe(data => console.log(data));
 
-// const debouncedValues$ = values$.debounceTime(300);
+const debouncedValues$ = values$.debounceTime(300);
 
-// // debouncedValues$.subscribe(debouncedValue => console.log(debouncedValue));
+// debouncedValues$.subscribe(debouncedValue => console.log(debouncedValue));
 
-// const onlyNeededLengthValues$ = debouncedValues$.filter(value => value.length >= 1);
+const onlyNeededLengthValues$ = debouncedValues$.filter(value => value.length >= 1);
 
-// // onlyNeededLengthValues$.subscribe(value => console.log(value));
+// onlyNeededLengthValues$.subscribe(value => console.log(value));
 
-// const items$ = onlyNeededLengthValues$
-// 	.mergeMap(value => Rx.Observable.fromPromise(request(value).promise));
+const items$ = onlyNeededLengthValues$
+    .mergeMap(value => Rx.Observable.fromPromise(request(value).promise));
 
-// items$
-// 	.subscribe(items => {
-// 		clearResult(results);
-// 		showResult(items, results);
-// 	});
-
-
-// Rx.Observable.fromEvent(input, 'keyup')
-//     .do(event => console.log('keyup', event))
-// 	.debounceTime(300)
-// 	.do(event => console.log('debounced event', event))
-// 	.map(event => event.target.value)
-// 	.do(value => console.log('mapped value', value))
-// 	.filter(value => value.length >= 1)
-// 	.do(value => console.log('filtered value', value))
-// 	.switchMap(value => Rx.Observable.fromPromise(request(value).promise))
-// 	.do(items => console.log('items' , items))
-// 	.subscribe(items => {
-// 		clearResult(results);
-// 		showResult(items, results);
-// 	});
+items$
+    .subscribe(items => {
+      clearResult(results);
+      showResult(items, results);
+    });
 
 
 Rx.Observable.fromEvent(input, 'keyup')
-	.debounceTime(300)
-	.map(event => event.target.value)
-	.filter(value => value.length >= 1)
-	.switchMap(value => Rx.Observable.fromPromise(request(value).promise))
-	.subscribe(items => {
-		clearResult(results);
-		showResult(items, results);
-	});
+    .do(event => console.log('keyup', event))
+    .debounceTime(300)
+    .do(event => console.log('debounced event', event))
+    .map(event => event.target.value)
+    .do(value => console.log('mapped value', value))
+    .filter(value => value.length >= 1)
+    .do(value => console.log('filtered value', value))
+    .switchMap(value => Rx.Observable.fromPromise(request(value).promise))
+    .do(items => console.log('items', items))
+    .subscribe(items => {
+      clearResult(results);
+      showResult(items, results);
+    });
+
+
+Rx.Observable.fromEvent(input, 'keyup')
+    .debounceTime(300)
+    .map(event => event.target.value)
+    .filter(value => value.length >= 1)
+    .switchMap(value => Rx.Observable.fromPromise(request(value).promise))
+    .subscribe(items => {
+      clearResult(results);
+      showResult(items, results);
+    });
